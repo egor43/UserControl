a0c7bf041ccf660fbbcafe663cdb7178c0350901
egor43 <egor43.rus@mail.ru>
2016-09-14 17:03:59 +0300

    Готово окончательно

--- UserControl_Myshko/UserControl_Myshko/MainWindow.xaml.cs
+++ UserControl_Myshko/UserControl_Myshko/MainWindow.xaml.cs
@@ -10,22 +10,41 @@ namespace UserControl_Myshko
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Инициализация компонентов
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
+        #region Обработчики событий
+
+        /// <summary>
+        /// Обрботчик события нажатия на цифровую клавишу в пользовательском элементе "TableButon"
+        /// </summary>
+        /// <param name="sender">Объект вызвавший событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void TableButton_NumClick(object sender, EventArgs e)
         {
             textBox.Text = SummBlock.IncomingSymbol(textBox.Text, ((Button)sender).Tag.ToString(), '#');
         }
 
+        /// <summary>
+        /// Обрботчик события нажатия на клавишу "Back" в пользовательском элементе "TableButon"
+        /// </summary>
+        /// <param name="sender">Объект вызвавший событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void TableButton_BackClick(object sender, EventArgs e)
         {
             textBox.Text = SummBlock.DeleteCharacter(textBox.Text, '#', '-');
         }
 
+        /// <summary>
+        /// Обрботчик события нажатия на клавишу "OK" в пользовательском элементе "TableButon"
+        /// </summary>
+        /// <param name="sender">Объект вызвавший событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void TableButton_OkayClick(object sender, EventArgs e)
         {
             if (SummBlock.CompareBlocks(textBox.Text, '-', 3)) MessageBox.Show("Сумма блоков равна!");
@@ -33,4 +52,5 @@ namespace UserControl_Myshko
             textBox.Text=textBox.Tag.ToString();
         }
     }
+    #endregion
 }
