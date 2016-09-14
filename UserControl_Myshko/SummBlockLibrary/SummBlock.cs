afb85f48db45fad50d9f8c2ff72c40d6f3fe7135
egor43 <egor43.rus@mail.ru>
2016-09-14 16:52:24 +0300

    Добавлена документация

--- UserControl_Myshko/SummBlockLibrary/SummBlock.cs
+++ UserControl_Myshko/SummBlockLibrary/SummBlock.cs
@@ -4,8 +4,19 @@ using System.Text.RegularExpressions;
 
 namespace SummBlockLibrary
 {
-    public class SummBlock
+    /// <summary>
+    /// Класс для работы с текстовым полем программы. 
+    /// </summary>
+    public static class SummBlock
     {
+        #region Методы класса
+        /// <summary>
+        /// Метод для последовательного добавления символа в строку
+        /// </summary>
+        /// <param name="TextOfTextBlock">Входная строка, которую необходимо заменить</param>
+        /// <param name="Input">Символ который необходимо вставить</param>
+        /// <param name="FillCharacter">Символ, заполняющий пустое место</param>
+        /// <returns>Возвращает измененную входную строку в которой первый встретившийся "FillCharacter" заменен на "Input"</returns>
         public static string IncomingSymbol(string TextOfTextBlock, string Input, char FillCharacter)
         {
             char[] DismemberedLine = TextOfTextBlock.ToCharArray();
@@ -25,6 +36,13 @@ namespace SummBlockLibrary
             return result;
         }
 
+        /// <summary>
+        /// Метод для последовательного удаления символа из строки по принципу: "последний добавлен - первый удален"
+        /// </summary>
+        /// <param name="TextOfTextBlock">Входная строка, которую необходимо заменить</param>
+        /// <param name="FillCharacter">Символ, заполняющий пустое место</param>
+        /// <param name="DelimiterCharacter">Символ, разделяющий блоки</param>
+        /// <returns>Возвращает измененную входную строку в которой последний добавленный символ заменен на "FillCharacter"</returns>
         public static string DeleteCharacter(string TextOfTextBlock, char FillCharacter, char DelimiterCharacter)
         {
             StringBuilder str = new StringBuilder(TextOfTextBlock);
@@ -39,6 +57,13 @@ namespace SummBlockLibrary
             return str.ToString();
         }
 
+        /// <summary>
+        /// Проверяет, равны ли суммы всех блоков
+        /// </summary>
+        /// <param name="TextOfTextBlock">Входная строка, содержащая блоки и разделители блоков</param>
+        /// <param name="DelimiterCharacter">Символ, разделяющий блоки</param>
+        /// <param name="NumberOfBlocks">Колличество блоков</param>
+        /// <returns>Истина - если сумма блоков равна. Ложь - если сумма блоков не равна</returns>
         public static bool CompareBlocks(string TextOfTextBlock, char DelimiterCharacter, int NumberOfBlocks)
         {
             int [] Blocks = new int[NumberOfBlocks];
@@ -64,6 +89,6 @@ namespace SummBlockLibrary
             if (result == NumberOfBlocks) return true;
             else return false;
         }
-
+        #endregion
     }
 }
--- UserControl_Myshko/WpfControlLibrary1/UserControl1.xaml.cs
+++ UserControl_Myshko/WpfControlLibrary1/UserControl1.xaml.cs
@@ -20,30 +20,71 @@ namespace WpfControlLibrary1
     /// </summary>
     public partial class UserControl1 : UserControl
     {
+        /// <summary>
+        /// Инициализация компонентов
+        /// </summary>
         public UserControl1()
         {
             InitializeComponent();
         }
+
+        #region Делегаты
+        /// <summary>
+        /// Делегат для создания пользовательских событий
+        /// </summary>
+        /// <param name="sender">Объект, вызвавший событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         public delegate void UserControlDelegate(object sender, EventArgs e);
+        #endregion
+
+        #region События
+        /// <summary>
+        /// Событие нажатия на цифровую клавишу
+        /// </summary>
         public event UserControlDelegate NumClick;
+        /// <summary>
+        /// Событие нажатия на клавишу "Back"
+        /// </summary>
         public event UserControlDelegate BackClick;
+        /// <summary>
+        /// Событие нажатия на клавишу "ОК"
+        /// </summary>
         public event UserControlDelegate OkayClick;
+        #endregion
+
+        #region Метобы вызова событий
+        /// <summary>
+        /// Метод вызова события "NumClick"
+        /// </summary>
+        /// <param name="sender">Объект, вызвавший событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void number_Click(object sender, RoutedEventArgs e)
         {
             if (NumClick != null)
                 NumClick(sender, e);
         }
 
+        /// <summary>
+        /// Метод вызова события "BackClick"
+        /// </summary>
+        /// <param name="sender">Объект, вызвавшйи событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
             if (BackClick != null)
                 BackClick(sender, e);
         }
 
+        /// <summary>
+        /// Метод вызова события "OkayClick"
+        /// </summary>
+        /// <param name="sender">Объект, вызвавшйи событие</param>
+        /// <param name="e">Дополнительные параметры</param>
         private void OKbutton_Click(object sender, RoutedEventArgs e)
         {
             if (OkayClick != null)
                 OkayClick(sender, e);
         }
+        #endregion
     }
 }
